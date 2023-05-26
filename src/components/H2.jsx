ea85d7c98b04b575f4eab7dc97959291f463fbd4
RdrBux <email_address>
2023-05-26 09:33:02 -0300

    Chance size of titles' text to fixed units on small screens to prevent overflow

--- src/components/H2.jsx
+++ src/components/H2.jsx
@@ -1,7 +1,7 @@
 export default function H2({ children, wrap = true }) {
   return (
     <h2
-      className={`-mt-8 font-serif text-6xl font-semibold uppercase lg:text-5xl xl:text-7xl ${
+      className={`-mt-8 font-serif text-[16vw] font-semibold uppercase sm:text-6xl lg:text-5xl xl:text-7xl ${
         wrap ? '' : 'whitespace-nowrap'
       }`}
     >
